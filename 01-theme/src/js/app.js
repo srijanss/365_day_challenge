7fb4021cf8a0562755f34b9be634b91e642f894e
Srijan Manandhar <srijan.manandhar@gmail.com>
2020-06-27 22:41:15 +1000

    Dropdown menu on hover on shop and contactus menu item

--- 01-theme/src/js/app.js
+++ 01-theme/src/js/app.js
@@ -1,3 +1,24 @@
 import "bootstrap";
 import "../sass/main.scss";
 import "./custom";
+
+const _shopDropdownLink = document.querySelector("#shop-dropdown-link");
+const _shopDropdown = document.querySelector(".shop__dropdown");
+const _contactUsDropdownLink = document.querySelector(
+  "#contact-us-dropdown-link"
+);
+const _contactUsDropdown = document.querySelector(".contact-us__dropdown");
+
+_shopDropdownLink.addEventListener("mouseover", () => {
+  _shopDropdown.style.setProperty("--k", 1);
+});
+_shopDropdownLink.addEventListener("mouseout", () => {
+  _shopDropdown.style.setProperty("--k", 0);
+});
+
+_contactUsDropdownLink.addEventListener("mouseover", () => {
+  _contactUsDropdown.style.setProperty("--k", 1);
+});
+_contactUsDropdownLink.addEventListener("mouseout", () => {
+  _contactUsDropdown.style.setProperty("--k", 0);
+});
